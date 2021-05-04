64e2fc41ab28cd905e57045624fb8ce33ffa4b98
EmiliaUrsu <74149102+EmiliaUrsu@users.noreply.github.com>
2021-05-04 20:17:06 +0300

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,52 @@
+
+function newBall(size, color) {
+    return {
+        size: size,
+        color: color,
+        x: 50,
+        y: 100,
+        speedX: 15,
+        speedY: 15,
+
+        render: function () {
+            return `
+                <div style="
+                    width:${this.size}px;
+                    height:${this.size}px;
+                    border-radius: ${this.size}px;
+                    background-color: ${this.color};
+                    position: absolute;
+                    left: ${this.x}px;
+                    top: ${this.y}px;
+                    
+                ">
+                </div>
+                `
+        },
+        renderInside: function (container) {
+            container.innerHTML = this.render()
+        },
+
+
+move: function () {
+    if (this.x + this.size >= innerWidth || this.x <= 0) {
+        this.speedX *= -1 }
+    this.x += this.speedX
+
+    if (this.y + this.size >= innerHeight || this.y <= 0) {
+        this.speedY *= -1 }
+    this.y += this.speedY
+},
+
+animate: function () {
+    let thisObject = this
+    setInterval(function () {
+        thisObject.move()
+        thisObject.renderInside(container)
+    }, 100)
+}
+}
+}
+
+let ball = newBall(100, "red")
+ball.animate()
